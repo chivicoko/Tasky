1cf45fd1d277fcf7bf55e073d6894122a1b96790
chivicoko <victor.c.okoye@gmail.com>
2024-06-21 14:39:08 +0100

    update templates

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
     path('tasks/', include('tasks.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    # path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from .models import Task
 from .forms import TaskForm
 
+@login_required
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/index.html', {'tasks': tasks})
 
+@login_required
 def task_detail(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     return render(request, 'tasks/task_detail.html', {'task': task})
 
+@login_required
 def task_create(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -22,6 +26,7 @@ def task_create(request):
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form})
 
+@login_required
 def task_edit(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     if request.method == 'POST':
@@ -33,6 +38,7 @@ def task_edit(request, task_id):
         form = TaskForm(instance=task)
     return render(request, 'tasks/task_form.html', {'form': form})
 
+@login_required
 def task_delete(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     if request.method == 'POST':
